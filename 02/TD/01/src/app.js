69821585e67c17877413eee922c4630d8f99f6fe
Cyrille Perois <cperois@outlook.com>
2019-01-30 17:32:48 +0100

    add 02 correction

--- 02/TD/01/src/app.js
+++ 02/TD/01/src/app.js
@@ -6,25 +6,65 @@ class Slider {
     this.currentIndex = 0
 
     this.goToNextSlide = this.goToNextSlide.bind(this)
+    this.goToPreviousSlide = this.goToPreviousSlide.bind(this)
 
     this.onInit()
   }
 
   onInit() {
     this.interval = setInterval(this.goToNextSlide, 2000)
+
+    this.createControls()
+  }
+
+  createControls() {
+    this.previousButton = document.createElement("button")
+    this.previousButton.innerHTML = "Previous"
+    this.root.append(this.previousButton)
+    this.previousButton.addEventListener("click", this.goToPreviousSlide)
+
+    this.nextButton = document.createElement("button")
+    this.nextButton.innerHTML = "Next"
+    this.root.append(this.nextButton)
+    this.nextButton.addEventListener("click", this.goToNextSlide)
   }
 
   destroy() {
-    clearInterval(this.interval)
+    this.stop()
+  }
+
+  stop() {
+    if (this.interval !== null) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
   }
 
   goToNextSlide() {
+    this.stop()
+
     ++this.currentIndex
 
     if (this.currentIndex === this.nbSlides) {
       this.currentIndex = 0
     }
 
+    this.moveSlides()
+  }
+
+  goToPreviousSlide() {
+    this.stop()
+
+    --this.currentIndex
+
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.nbSlides - 1
+    }
+
+    this.moveSlides()
+  }
+
+  moveSlides() {
     const transform = `${this.currentIndex * -100}%`
     this.slidesContainer.style.transform = `translateX(${transform})`
   }
